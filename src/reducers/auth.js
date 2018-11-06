280bd1e6c338463e03b3ebd0f0a5a27910f80f12
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2018-11-06 21:25:02 +0300

    create store for authentication

---
+++ src/reducers/auth.js
@@ -0,0 +1,14 @@
+const defaultAuthState = { };
+
+export default (state=defaultAuthState, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        uid: action.uid
+      };
+    case 'LOGOUT':
+      return { };
+    default:
+      return state;
+  }
+};
